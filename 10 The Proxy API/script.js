25121dc859ddb0e1ace6a8ed3d54cdf91b6c5903
eslessons <66363651+eslessons@users.noreply.github.com>
2020-07-06 13:13:51 +0300

    10 The Proxy API is in process

--- 10 The Proxy API/script.js
+++ 10 The Proxy API/script.js
@@ -17,3 +17,134 @@
  *  this proxy. We have to setup certain Traps for obj
  *  e.g 'get' Trap that prevents direct access to obj.
  *                                                      */
+
+// TRAPS IN ACTION //
+
+(() => {
+  // let person = {
+  //   age: 27,
+  // };
+  // let handler = {
+  //   get: function (target, name) {
+  //     return name in target ? target[name] : 'Non existant';
+  //   },
+  // };
+  // var proxy = new Proxy(person, handler);
+  // console.log(person.age); // 27
+  // console.log(proxy.age); // 27
+  // console.log(person.name); // undefined
+  // console.log(proxy.name); // "Non existant"
+})();
+
+// PROXIES AND REFLECT //
+
+(() => {
+  // let person = {
+  //   age: 27,
+  //   name: 'Max',
+  // };
+  // let handler = {
+  //   get: function (target, name) {
+  //     return name in target ? target[name] : 'Non existant';
+  //   },
+  //   set: function (target, property, value) {
+  //     if (value.length >= 3) {
+  //       Reflect.set(target, property, value);
+  //     }
+  //   },
+  // };
+  // var proxy = new Proxy(person, handler);
+  // console.log(person.age); // 27
+  // console.log(proxy.age); // 27
+  // console.log(person.name); // "Max"
+  // console.log(proxy.name); // "Max"
+  // proxy.name = 'Lu';
+  // console.log(proxy.name); // "Max"
+  // proxy.name = 'Maximilian';
+  // console.log(proxy.name); // "Max"
+})();
+
+// USING PROXIES AS PROTOTYPES
+
+(() => {
+  // let person = {
+  //   age: 27,
+  //   name: 'Max',
+  // };
+  // let handler = {
+  //   get: function (target, name) {
+  //     return name in target ? target[name] : 'Non existant';
+  //   },
+  // };
+  // // Empty object instead of person
+  // let proxy = new Proxy({}, handler);
+  // console.log(proxy.name); // "Non existant"
+  // // Proxies can be "__proto__"
+  // Reflect.setPrototypeOf(person, proxy);
+  // console.log(person);
+  // // v {age: 27, name: "Max"}
+  // //    age: 27
+  // //    name: "Max"
+  // //  > __proto__: Proxy
+  // console.log(person.name); // "Max"
+  // console.log(person.hobbies); // "Non existant"
+})();
+
+// PROXIES AS PROXIES //
+
+(() => {
+  // let person = {
+  //   age: 27,
+  //   name: 'Max',
+  // };
+  // let handler = {};
+  // let protoHandler = {
+  //   get: function (target, name) {
+  //     return name in target ? target[name] : 'Non existant';
+  //   },
+  // };
+  // let proxy = new Proxy({}, handler);
+  // let protoProxy = new Proxy(proxy, protoHandler);
+  // Reflect.setPrototypeOf(person, protoProxy);
+  // console.log(person);
+  // // v {age: 27, name: "Max"}
+  // //    age: 27
+  // //    name: "Max"
+  // //  > __proto__: Proxy
+  // console.log(person.name); // "Max"
+  // console.log(person.hobbies); // "Non existant"
+})();
+
+// WRAPPING FUNCTIONS //
+
+(() => {
+  // function log(message) {
+  //   console.log('Log entry created, message: ' + message);
+  // }
+  // log('Hello!'); // "Log entry created, message: Hello!"
+  // let handler = {
+  //   // apply trap
+  //   apply: function (target, thisArg, argumentsList) {
+  //     if (argumentsList.length == 1) {
+  //       return Reflect.apply(target, thisArg, argumentsList);
+  //     }
+  //   },
+  // };
+  // let proxy = new Proxy(log, handler);
+  // proxy('Hello from Proxy'); // Log entry created, message: Hello from Proxy
+  // proxy('Hello', 10); // logs nothing (argumentsList.length > 1)
+})();
+
+// REVOCABLE PROXIES //
+
+(() => {})();
+
+(() => {})();
+
+(() => {})();
+
+(() => {})();
+
+(() => {})();
+
+(() => {})();
